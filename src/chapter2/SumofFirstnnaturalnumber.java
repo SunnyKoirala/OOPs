45b371081db4ce6f458fc35d8f2f4bf80815b8fe
Sunny Koirala <sunnykoirala777@gmail.com>
2025-11-27 18:56:51 +0545

    made some changes

--- src/chapter2/SumofFirstnnaturalnumber.java
+++ src/chapter2/SumofFirstnnaturalnumber.java
@@ -1,5 +1,16 @@
 package chapter2;
-
+import java.util.*;
 public class SumofFirstnnaturalnumber {
-
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while (i <= num) {
+            sum += i;
+            i++;
+        }
+        System.out.println("The sum is: " + sum);
+    }
 }
